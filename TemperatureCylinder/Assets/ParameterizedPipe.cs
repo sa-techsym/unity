a038a036e4e12c65f43ac1b0e1d55de87264ebb6
sa-techsym <65368149+sa-techsym@users.noreply.github.com>
2020-09-13 11:27:27 +0300

    Add files via upload

--- TemperatureCylinder/Assets/ParameterizedPipe.cs
+++ TemperatureCylinder/Assets/ParameterizedPipe.cs
@@ -31,16 +31,22 @@ public class ParameterizedPipe : MonoBehaviour
 			{
 				if ( _mesh == null )
 					return;
+				
+				for ( int i = 0; i < _mesh.vertices.Length; i ++ )
+					{
+						Gizmos.color = (_mesh.vertices [i].z < 0) ? Color.black : Color.yellow;
+						Gizmos.DrawSphere (_mesh.vertices [i], 0.1f);
+					}
 
-				Gizmos.color = Color.red;	
+				Gizmos.color = Color.red;
 
 				for ( int i = 0; i < _mesh.vertices.Length; i ++ )
-						Gizmos.DrawLine (_mesh.vertices [i], _mesh.normals [i]);
+						Gizmos.DrawRay (_mesh.vertices [i], _mesh.normals [i]);
 			}
 
 		private Mesh _mesh = null;
 
-		protected virtual void Awake ()
+		private void Awake ()
 			{
 				GetComponent <MeshFilter> ().mesh = _mesh = new Mesh ();
 			
@@ -51,43 +57,14 @@ public class ParameterizedPipe : MonoBehaviour
 				_mesh.triangles = triangles ();
 		
 				// ...
-				_mesh.RecalculateNormals ();
+				//_mesh.RecalculateNormals ();
+				_mesh.normals = normals ();
 				
-				//NormalSolver.RecalculateNormals (_mesh, 180);
-
-				//Vector3 [] normals = _mesh.normals;
-				
-				// artificial normal smoothing
-				//for ( int i = 0; i < normals.Length; i ++ )
-				//		normals [i].x = normals [i].y = 0;
-		
-				//_mesh.normals = normals;
-
-				// alternative normal calculation method
-				//_mesh.normals = calculate_normals ();
-
-				
-				float thickness = _outerRadius - _innerRadius;
-
-				Vector2 [] uv = new Vector2 [_mesh.vertices.Length];
-
-				int i = 0;
-
-			//float [] step = {thickness, _length, thickness, +length}; 
-
-				for ( float y = 0; y < 2f * Mathf.PI; y += (2f * Mathf.PI) / _segmentCount )
-						for ( float x = 0;  x < 2 * thickness + 4 * _length; x += (i % 2 == 0) ? thickness : 2 * _length )
-							{
-			 					uv [i] = new Vector2 (x / (2 * thickness + 4 * _length), y / (2f * Mathf.PI));
-								i ++;
-							}
-				
-				_mesh.uv = uv;
-
-				
+						
+				_mesh.uv = uv ();
 			}
 			
-		protected virtual Vector3 [] vertices ()
+		private Vector3 [] vertices ()
 			{
 				List <Vector3> result = new List <Vector3> (); // [_segment_count * 4];
 				
@@ -106,7 +83,7 @@ public class ParameterizedPipe : MonoBehaviour
 				return result.ToArray ();
 			}
 
-		protected List <int> pattern (int delta)
+		private List <int> pattern (int delta)
 			{
 				List <int> 
 						pattern = new List <int> 
@@ -119,7 +96,7 @@ public class ParameterizedPipe : MonoBehaviour
 				return result;
 			}
 
-		protected virtual int [] triangles ()
+		private int [] triangles ()
 			{
 				List <int> result = new List <int> ();
 				
@@ -130,7 +107,7 @@ public class ParameterizedPipe : MonoBehaviour
 						result.AddRange (pattern (i / 6)); // must be 4  
 						i += 24;
 					}
-
+				
 				// trim last six indices
 				for ( i = (_segmentCount - 1) * 24; i < _segmentCount * 24; i ++ )
 						result [i] = result [i] >= _segmentCount * 4 
@@ -139,4 +116,51 @@ public class ParameterizedPipe : MonoBehaviour
 										
 				return result.ToArray ();
 			}
+
+		private Vector3 [] normals ()
+			{
+				Vector3 [] normal = new Vector3 [_mesh.vertices.Length];
+
+                float angle = 0;
+
+               	for ( int i = 0; i < _mesh.vertices.Length; i += 4 )
+				    {
+                        // ...
+                        Vector3 indir = new Vector3 (- _innerRadius * Mathf.Cos (angle),
+                                                     - _innerRadius * Mathf.Sin (angle), _innerRadius * _innerRadius / _length);
+
+                        normal [i + 0] = new Vector3 (indir.x, indir.y, - indir.z).normalized;
+					    normal [i + 3] = new Vector3 (indir.x, indir.y, + indir.z).normalized;
+
+                        // ...
+                        Vector3 outdir = new Vector3 (_outerRadius * Mathf.Cos (angle), 
+                                                      _outerRadius * Mathf.Sin (angle), _outerRadius * _outerRadius / _length);
+
+                        normal [i + 1] = new Vector3 (outdir.x, outdir.y, - outdir.z).normalized;
+                        normal [i + 2] = new Vector3 (outdir.x, outdir.y, + outdir.z).normalized;
+
+                        angle += (2f * Mathf.PI) / _segmentCount; 
+                    }
+
+				return normal;
+			}
+
+        private Vector2 [] uv ()
+            {
+                Vector2 [] uv = new Vector2 [_mesh.vertices.Length];
+
+                float wall = _outerRadius - _innerRadius; 
+                
+                int i = 0;
+
+                for ( float y = 0; y < 2f * Mathf.PI; y += (2f * Mathf.PI) / _segmentCount )
+                        for ( float x = 0;
+                              x < 2 * wall + 4 * _length; x += (i % 2 == 0) ? wall : 2 * _length )
+                            {
+                                uv [i] = new Vector2 (x / (2 * wall + 4 * _length), y / (2f * Mathf.PI));
+                                i ++;
+                            }
+                
+                return uv;
+            }
 	}
